e43e4b3d6b2e4ff44bfde71b8b4d659183f3c761
Ronisko <koripier@gmail.com>
2017-05-21 17:27:48 +0200

    Add alghoritm

--- master.c
+++ master.c
@@ -1,27 +1,6 @@
 #include "def.h"
 #include <sys/time.h>
 
-main()
-{
-	int mytid;
-	int tids[SHIPSNUM];
-	int neededTowboats[SHIPSNUM];
-	int nproc, i, who;
-
-	nproc = pvm_spawn(SHIPNAME, NULL, PvmTaskDefault, "", SHIPSNUM, tids);
-
-	for( i = 0 ; i < nproc ; i++ )
-	{
-		pvm_initsend(PvmDataDefault);
-		pvm_pklong(current_timestamp(), 1, 1);
-		pvm_pkint(neededTowboats[i], 1, 1);
-		pvm_pkint(&tids, )
-	   	pvm_send(tids[i], MSG_MSTR);
-	}
-
-	pvm_exit();
-}
-
 long long current_timestamp() {
     struct timeval te; 
     gettimeofday(&te, NULL); // get current time
@@ -29,3 +8,36 @@ long long current_timestamp() {
     // printf("milliseconds: %lld\n", milliseconds);
     return milliseconds;
 }
+
+main()
+{
+	int i;
+	int numberOfShips = 5;
+	int numberOfTowboats = 10;
+
+	// printf("Podaj liczbe statkow.\n");
+	// scanf("%d", &numberOfShips);
+	// for ( i = 0; i < numberOfShips; i++ ) {
+	// 	printf("Podaj liczbe potrzebnych holowników dla statku nr %d.\n", i);
+	// 	scanf("%d", &numberOfShips);
+	// }
+
+	int tids[5];
+	int neededTowboats[5] = {5, 8, 2, 5, 10};
+	int nproc;
+
+	nproc = pvm_spawn(SHIPNAME, NULL, PvmTaskDefault, "", numberOfShips, tids);
+
+	for( i = 0 ; i < nproc ; i++ )
+	{
+		pvm_initsend(PvmDataDefault);
+		pvm_pkint(SHIPSNUM, 1, 1); // obsluzone
+		pvm_pklong(current_timestamp(), 1, 1); //obsluzone
+		pvm_pkint(neededTowboats[i], 1, 1); //obsluzone
+		pvm_pkint(&tids, SHIPSNUM, 1); // obsluzone
+	   	pvm_send(tids[i], MSG_MSTR);
+	}
+
+	pvm_exit();
+}
+
--- slave.c
+++ slave.c
@@ -1,37 +1,51 @@
 #include "def.h"
 
-void e_send( int to, char *message ) {
+void e_send( int to, int message ) {
+	pvm_initsend(PvmDataDefault);
+	switch( message ) {
+		case 1:
+			pvm_send(to, PERMISSION);
+			break;
+		case 2:
+			pvm_send(to, REQUEST);
+			break;
+		case 3:
+			pvm_send(to, ENTRY);
+			break;
+		case 4:
+			pvm_send(to, RELEASE);
+			break;
+	}
 
 
-
-//tutaj cos z pvm
 }
 
 void e_receive() {
-
-//tutaj cos z pvm
-
-	if (pcktIn.type == "PERMISSION") {
-		zgody.push(Qj); 
+	int j, receivedPriority;
+	int towboats[];
+	if ( pvm_nrecv(-1, PERMISSION) ) {
+		pvm_upkint(&j, 1, 1);
+		zgody[j] = true; 
 	}
-	// jeśli otrzymaliśmy "ŻĄDANIE":
-	else if (pcktIn.type == "REQUEST") {
-		statkiAktywne.push(Qj);
+	else if ( pvm_nrecv(-1, REQUEST) ) {
+		pvm_upkint(&j, 1, 1);
+		pvm_upkint(&receivedPriority, 1, 1);
+		statkiAktywne[j] = true;
 		if (statkiAktywne[i] == true) {
 			
-			if (pcktIn.priorytet > priorytet) {  
-				e_send(Qi, Qj, "PERMISSION");
+			if (receivedPriority > priority) {  
+				e_send(Qi, Qj, PERMISSION);
 			}
 		}
 	}
-	// jeśli otrzymaliśmy "ZWOLNIENIE":
-	else if (pcktIn.type == "ZWOLNIENIE") {
-		holownikiDostępne.push(pcktIn.holowniki); // dodajemy zwolnione holowniki do dostępnych
+	else if ( pvm_nrecv(-1, REQUEST) ) {
+		pvm_upkint(towboats, SIZE, 1);
+		holownikiDostępne.push(pcktIn.holowniki);
 	}
-	// jeśli otrzymaliśmy "WEJŚCIE":
-	else if (pcktIn.type == "ENTRY") {
-		holownikiDostępne.remove(pcktIn.holowniki) // usuwamy zagarnięte holowniki z dostępnych
-		statkiAktywne.remove(Qj); // usuwamy statek będący w sekcji krytycznej z aktywnych
+	else if ( pvm_nrecv(-1, ENTRY) ) {
+		pvm_upkint(&j, 1, 1);
+		holownikiDostępne.remove(pcktIn.holowniki)
+		statkiAktywne[j] = false;
 	}
 }
 
@@ -42,51 +56,68 @@ void deletePermissions(bool *permissions, int number) {
 	}
 }
 
+bool equalArrays(int tab[], int tab2[], int length) {
+    for (int i = 0; i < length; i++)
+        if (tab[i] != tab2[i])
+            return false;
+
+    return true;
+}
+
 main()
 {
+	int numberOfShips;
+	long long priority;
 	int neededTowboats;
-	char slave_name[NAMESIZE];
+	int ships[];
 
-	mytid = pvm_mytid();
+	int mytid = pvm_mytid();
 	gethostname(slave_name, NAMESIZE);
 
+	/**
+	  * internal
+	  */
 	pvm_recv( -1, MSG_MSTR );
-	pvm_upkint(&first, 1, 1);
+	pvm_upkint(&numberOfShips, 1, 1); //ok
+	pvm_upkint(&priority, 1, 1); // ok
+	pvm_upkint(&neededTowboats, 1, 1); // ok
+	pvm_upkint(&ships, numberOfShips, 1); // ok
 	pvm_upkint(&tid, 1, 1 );
 	pvm_upklong(&neededTowboats, 1, 1)
+
+	/* */
 
 	bool permissions[];
 	int availableTowboats[];
 	int ownedTowboats[];
 	int reservedTowboats[];
-	bool activeShips;
-	int priority;
 	bool ready;
 
 
 	while (true) {
 		//sekcja lokalna()
+
 		e_send("REQUEST", neededTowboats, ownedTowboats, priority);
 		ready = false;
 		deletePermissions(permissions);
 
 		while( !ready ) {
-		e_receive();
+			e_receive();
 
-		if ( zgody.length == statkiAktywne.length ) {
-			gotowy = true;
-			while ( true ) {
-				holownikiZarezerwowane = holownikiDostępne.slice ( liczbaPotrzebnychHolowników )
-				if ( liczbaPotrzebnychHolowników != holownikiZarezerwowane.length() ) {
-					break; 
+			if ( equalArrays(zgody, statkiAktywne, ) ) {
+				gotowy = true;
+				while ( true ) {
+					holownikiZarezerwowane = holownikiDostępne.slice ( liczbaPotrzebnychHolowników )
+					if ( liczbaPotrzebnychHolowników != holownikiZarezerwowane.length() ) {
+						break; 
+					}
+					e_receive();
 				}
-				e_receive();
-			}
-			holownikiKtoreMam.push(holownikiZarezerwowane);
-			holownikiDostępne.remove(holownikiZarezerwowane);
-			e_send(Qi, *, "ENTRY");
-		} 
-	}
+				holownikiKtoreMam.push(holownikiZarezerwowane);
+				holownikiDostępne.remove(holownikiZarezerwowane);
+				e_send(Qi, *, "ENTRY");
+			} 
+		}
 	}
 
 
